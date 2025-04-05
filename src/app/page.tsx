f07628ae7d510d79aec6e0a20fcb6ceb57162f07
megzzawy <amgadsameh99@gmail.com>
2025-04-05 07:35:48 +0200

    adding prject section structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,32 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
-      {/* Header */}
       <Header />
-
-      {/* Hero */}
 
       <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
 
-      {/* Experience */}
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
 
-      {/* Skills */}
       <section id="skills" className="snap-start">
         <Skills/>
       </section>
 
-      {/* Projects */}
-
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact Me */}
     </div>
   );
---
+++ src/components/Projects.tsx
@@ -0,0 +1,54 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+export default function Projects({}: Props) {
+    const projects = [1,2,3,4,5];
+  return (
+    <motion.div 
+    initial={{
+        opacity: 0,
+    }}
+    whileInView={{
+        opacity: 1,
+    }}
+    transition={{ duration: 1.5 }}
+    className='h-screen relative flex flex-col overflow-hidden text-center md:text-left xl:flex-row 
+    max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
+
+        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+            {projects.map((project,i) => (
+                <div className='w-12/12  snap-start flex flex-col flex-shrink-0 space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+                    <motion.img
+                        initial={{
+                            y: -300,
+                            opacity: 0,
+                        }}
+                        transition={{ duration: 1.2 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                    src="mockup.png" alt="" className='lg:w-92'/>
+
+                    <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+                        <h4 className='text-4xl font-semibold text-center'>
+                            <span className='underline decoration-[#F7AB0A]/50'>Case Study {i + 1} of {projects.length}:</span>
+                             UPS Clone
+                        </h4>
+                        <p className='text-lg text-center md:text-left'>
+                            UPS is a global logistics company that delivers packages and products to various locations.
+                            This project involved designing and developing a clone of UPS's website and mobile application.
+                            This project involved designing and developing a clone of UPS's website and mobile application.
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+      <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
+    </motion.div>
+  )
+}
