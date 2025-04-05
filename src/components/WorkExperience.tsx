6d38f9ece98be0b2c1f1619ee00bef95f4c6f25e
megzzawy <amgadsameh99@gmail.com>
2025-04-05 09:27:18 +0200

    adding scrollbar styling

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -16,7 +16,8 @@ export default function WorkExperience({}: Props) {
     transition={{ duration: 1.5 }}
      className='h-screen relative overflow-hidden flex  flex-col text-left md:text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20
+     scrollbar-thumb-[#F7AB0A]/80 '>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
